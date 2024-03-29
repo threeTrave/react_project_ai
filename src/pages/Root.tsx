0f479e3bcae8ec90535e4b4f701596868264d590
Nevermore <mengmiao.0101@icloud.com>
2024-03-30 01:01:47 +0800

    样式修改

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Footer } from '../components/Footer';
 
 export const Root = () => {
-  return (
-    <div>Root</div>
-  )
-}
+	return (
+		<div className='flex flex-col h-screen'>
+			<div className='border-solid border-2 border-yellow-500 flex-1' >Root</div>
+			<Footer></Footer>
+		</div>
+	);
+};
