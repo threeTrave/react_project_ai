62c28bf4cf5616e8d496c8c31a6570f3421b2343
Nevermore <mengmiao.0101@icloud.com>
2024-04-01 03:37:55 +0800

    修改表单组件

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,70 +1,40 @@
-import { useRef } from 'react'
+import { Rules } from 'async-validator'
 import { Footer } from '../components/Footer'
-import { ZxxrForm, ZxxrFormItem } from '../components/ZxxrForm.tsx'
+import { ZxxrForm } from '../components/ZxxrForm'
 export const RegisterPage = () => {
-	const modelRef = useRef({ name: '', pwd: '', pwdReapt: '', phone: '' })
-	// const [modelRef, model, setModel] = useZxxrState({ name: '', pwd: '', pwdReapt: '', phone: '' })
-	const rules = {
+	const rules: Rules = {
 		name: {
 			required: true,
 			type: 'string',
+			message: '请输入用户名'
 		},
 		pwd: {
 			required: true,
 			type: 'string',
+			message: '请输入密码'
 		},
 		pwdReapt: {
 			required: true,
 			type: 'string',
+			message: '请输入确认密码'
 		},
 		phone: {
 			required: true,
 			type: 'string',
-		},
+			message: '请输入手机号'
+		}
 	}
 	return (
-		<div className="flex flex-col h-screen">
-			<div className="border-solid border-2  flex-1 flex flex-row items-center justify-center">
-				<div className="w-4/5 h-2/3 bg-white flex">
+		<div className='flex flex-col h-screen'>
+			<div className='border-solid border-2  flex-1 flex flex-row items-center justify-center'>
+				<div className='w-4/5 h-2/3 bg-white flex'>
 					{/* 表单 */}
-					<ZxxrForm className="w-4/5 mr-12  text-black border-2 border-black" model={modelRef} rules={rules}>
-						<ZxxrFormItem className="pb-5" prop="name">
-							<input
-								className="border-2 border-black"
-								onChange={e => (modelRef.current.name = e.target.value)}
-								type="text"
-							/>
-						</ZxxrFormItem>
-						<ZxxrFormItem className="pb-5" prop="pwd">
-							<input
-								className="border-2 border-black"
-								onChange={e => (modelRef.current.pwd = e.target.value)}
-								type="text"
-							/>
-						</ZxxrFormItem>
-						<ZxxrFormItem className="pb-5" prop="pwdReapt">
-							<input
-								className="border-2 border-black"
-								onChange={e => (modelRef.current.pwdReapt = e.target.value)}
-								type="text"
-							/>
-						</ZxxrFormItem>
-						<ZxxrFormItem className="pb-5" prop="phone">
-							<input
-								className="border-2 border-black"
-								onChange={e => (modelRef.current.phone = e.target.value)}
-								type="text"
-							/>
-						</ZxxrFormItem>
-						<ZxxrFormItem>
-							<button type="submit">提交</button>
-						</ZxxrFormItem>
-					</ZxxrForm>
+					<ZxxrForm className='w-4/5 mr-12  text-black border-2 border-black' rules={rules} />
 					{/* 封面 */}
 					<img
-						className="object-center object-cover w-1/4"
-						src="https://img.zcool.cn/community/010d5c5b9d17c9a8012099c8781b7e.jpg@1280w_1l_2o_100sh.jpg"
-						alt=""
+						className='object-center object-cover w-1/4'
+						src='https://img.zcool.cn/community/010d5c5b9d17c9a8012099c8781b7e.jpg@1280w_1l_2o_100sh.jpg'
+						alt=''
 					/>
 				</div>
 			</div>
